a61e7e94b6d799779a65608ca69cd74e95019edd
Eduardo Gonçalves <eduardofcbg@gmail.com>
2020-04-15 23:21:48 +0100

    stdin

--- src/app/process.py
+++ src/app/process.py
@@ -2,11 +2,11 @@ from asyncio import create_subprocess_shell, create_task, CancelledError
 from subprocess import PIPE
 
 
-async def execute(command, input_document):
+async def execute(command, stdin):
     process = await create_subprocess_shell(
         command, stdin=PIPE, stdout=PIPE, stderr=PIPE
     )
-    communitation = create_task(process.communicate(input_document))
+    communitation = create_task(process.communicate(stdin))
 
     try:
         return await communitation
